0f21746b9566453bcae5efce14275e4b082b41c1
Atash <narzullaota@gmaial.com>
2024-01-13 22:44:51 -0500

    first commit

---
+++ src/Uy.java
@@ -0,0 +1,17 @@
+public class Uy {
+    static void checkAge(int age) {
+        if (age < 18) {
+            System.out.println("FUCK OFF");
+        } else {
+            System.out.println("FUCK ON");
+            String name =  ("Hello");
+            System.out.println(name);
+        }
+
+    }
+}
+
+    public static void main(String[] args) {
+        checkAge(20);
+    }
+}
